617f88dd809a006bac1f647510ffd15eaf2b826a
adityaudupa93 <aditya.udupa@gmail.com>
2023-09-20 19:38:11 +0200

    Final code

--- tennis_ace_starting/script.py
+++ tennis_ace_starting/script.py
@@ -22,90 +22,79 @@ print("------------NaNs--------------")
 print("Number of NaNs in each column")
 print(tennis_data.isna().any())
 
+
+
+# perform exploratory analysis here:
+
 # Seperating columns
 # 
 # print(tennis_data.loc[:, tennis_data.columns[(tennis_data.columns.str.startswith('First')) | (tennis_data.columns.str.startswith('Win'))]].head())
 
 
 # Pairplots for all variables
-sns.pairplot(tennis_data.loc[:, tennis_data.columns[(tennis_data.columns.str.startswith('First')) | (tennis_data.columns.str.startswith('Win'))]], hue="Winnings")
+relate_var = 'First'
+
+# sns.pairplot(tennis_data.loc[:, tennis_data.columns[(tennis_data.columns.str.startswith(relate_var)) | (tennis_data.columns.str.startswith('Win'))]])
+# plt.show()
+# plt.clf()
+
+# sns.pairplot(tennis_data.loc[:, tennis_data.columns[(tennis_data.columns.str.startswith(relate_var)) | (tennis_data.columns.str.startswith('Loss'))]])
+# plt.show()
+# plt.clf()
+
+# sns.pairplot(tennis_data.loc[:, tennis_data.columns[(tennis_data.columns.str.startswith(relate_var)) | (tennis_data.columns.str.startswith('Rank'))]])
+# plt.show()
+# plt.clf()
+
+
+
+## perform single and two feature linear regressions here:
+feat = ['ReturnGamesPlayed', 'BreakPointsOpportunities']
+outc = ['Winnings']
+features = tennis_data[feat]
+outcome = tennis_data[outc]
+
+features_train, features_test, outcome_train, outcome_test = train_test_split(features, outcome, train_size = 0.8)
+
+model = LinearRegression()
+model.fit(features_train,outcome_train)
+
+print("R2 score of predicting {} from {}: {}".format(outc, feat, model.score(features_test,outcome_test)))
+
+
+prediction = model.predict(features_test)
+plt.scatter(outcome_test,prediction, alpha=0.4)
+plt.title("Predicting {} from {}".format(outc, feat))
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
 plt.show()
 
 
-
-
-
-
-
-
-
-# perform exploratory analysis here:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## perform single feature linear regressions here:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## perform two feature linear regressions here:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ## perform multiple feature linear regressions here:
+
+
+feat = ['FirstServe','FirstServePointsWon','FirstServeReturnPointsWon',
+'SecondServePointsWon','SecondServeReturnPointsWon','Aces',
+'BreakPointsConverted','BreakPointsFaced','BreakPointsOpportunities',
+'BreakPointsSaved','DoubleFaults','ReturnGamesPlayed','ReturnGamesWon',
+'ReturnPointsWon','ServiceGamesPlayed','ServiceGamesWon','TotalPointsWon',
+'TotalServicePointsWon']
+outc = ['Winnings']
+features = tennis_data[feat]
+outcome = tennis_data[outc]
+
+features_train, features_test, outcome_train, outcome_test = train_test_split(features, outcome, train_size = 0.8)
+
+model = LinearRegression()
+model.fit(features_train,outcome_train)
+
+print("R2 score of predicting {} from {}: {}".format(outc, feat, model.score(features_test,outcome_test)))
+print("Model parameters: {}".format(model.coef_))
+
+
+prediction = model.predict(features_test)
+plt.scatter(outcome_test,prediction, alpha=0.4)
+plt.title("Predicting {} from {}".format(outc, feat))
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
+plt.show()
